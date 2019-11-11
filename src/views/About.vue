22132adc07a95a11d65d44c3155900a13e6ca59c
cailinfeng <cailinfeng@mobanker.com>
2019-11-11 22:13:43 +0800

    配置打包相对路径

--- src/views/About.vue
+++ src/views/About.vue
@@ -15,52 +15,54 @@
     }
 </style>
 <template>
-    <section class="hero"  >
-        <div class="hero-body" >
-            <div class="container">
-                <div class="tile">
-                    <b-navbar class="is-transparent navbar is-fixed-top" >
-                        <template slot="start">
-                            <div class="field is-grouped">
-                                <b-navbar-item class="has-text-white" href="/" >
-                                    <b-icon pack="fas" icon="angle-double-left"></b-icon>
-                                    <span >Home</span>
-                                </b-navbar-item>
-                            </div>
-                        </template>
-                    </b-navbar>
-                </div>
-                <div class="level ">
-                    <div class="level-item container has-text-left has-text-white is-narrow" style="padding-top: 200px" >
-                        <div class="container">
-                            <article class="content has-text-white">
-                                <p class="title is-large has-text-white">Lynn Chae</p>
-                                <p class="sub-title is-large">A Programmer</p>
-                                <p>Hi，我是一位来自魔都的程序员，这是我的第一个简单博客网站，很久没接触前端，springboot+thymeleaf也是第一次尝试，还请各位多多指教.....</p>
+    <div>
+        <section class="hero"  >
+            <div class="hero-body" >
+                <div class="container">
+                    <div class="tile">
+                        <b-navbar class="is-transparent navbar is-fixed-top" >
+                            <template slot="start">
+                                <div class="field is-grouped">
+                                    <b-navbar-item class="has-text-white" href="/" >
+                                        <b-icon pack="fas" icon="angle-double-left"></b-icon>
+                                        <span >Home</span>
+                                    </b-navbar-item>
+                                </div>
+                            </template>
+                        </b-navbar>
+                    </div>
+                    <div class="level ">
+                        <div class="level-item container has-text-left has-text-white is-narrow" style="padding-top: 200px" >
+                            <div class="container">
+                                <article class="content has-text-white">
+                                    <p class="title is-large has-text-white">Lynn Chae</p>
+                                    <p class="sub-title is-large">A Programmer</p>
+                                    <p>Hi，我是一位来自魔都的程序员，这是我的第一个简单博客网站，很久没接触前端，springboot+thymeleaf也是第一次尝试，还请各位多多指教.....</p>
 
-                            </article>
-                            <div>
-                                <nav class=" breadcrumb" aria-label="breadcrumbs">
-                                    <ul>
-                                        <li><a href="/contact"><font-awesome-icon class="has-text-white" :icon="['fas','at']"/></a></li>
-                                        <li><a href="https://www.weibo.com/lynnchae" target="_blank"><font-awesome-icon class="has-text-white" :icon="['fab','weibo']"/></a></li>
-                                        <li><a href="/contact"><font-awesome-icon class="has-text-white" :icon="['fab','weixin']"/></a></li>
-                                    </ul>
-                                </nav>
+                                </article>
+                                <div>
+                                    <nav class=" breadcrumb" aria-label="breadcrumbs">
+                                        <ul>
+                                            <li><a href="./contact.html"><font-awesome-icon class="has-text-white" :icon="['fas','at']"/></a></li>
+                                            <li><a href="https://www.weibo.com/lynnchae" target="_blank"><font-awesome-icon class="has-text-white" :icon="['fab','weibo']"/></a></li>
+                                            <li><a href="。/contact.html"><font-awesome-icon class="has-text-white" :icon="['fab','weixin']"/></a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="container">
-
-            </div>
-
-
-
-        </div>
-    </section>
+        </section>
+    </div>
 </template>
 <script>
+    export default {
+        data(){
+            return {
+            };
+        }
+    }
 </script>
 
